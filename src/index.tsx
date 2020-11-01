6d359a9e53406bbd2cecc35cad8fe1122ab628df
Yann <ydz0l038@gmail.com>
2020-11-01 20:45:16 +0100

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,6 @@ ReactDOM.render(<AppContextProvider><App /></AppContextProvider>, document.getEl
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 defineCustomElements(window)

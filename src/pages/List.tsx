42f7d86ceb3b4c1161200e8536c019da46897169
Yann <ydz0l038@gmail.com>
2020-11-22 17:31:01 +0100

    Add UX when no apartment

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -37,18 +37,23 @@ const List: React.FC = () => {
               <IonGrid>
                 <IonRow>
                   {
-                    appCtx.apartments.map((apartment, index) => (
-                      <IonCol size="12" sizeSm="6" sizeXl="4" key={index}>
-                        <ApartmentCard apartmentId={apartment.id} />
-                      </IonCol>
-                    ))
+                    appCtx.apartments.length > 0 ?
+                      appCtx.apartments.map((apartment, index) => (
+                        <IonCol size="12" sizeSm="6" sizeXl="4" key={index}>
+                          <ApartmentCard apartmentId={apartment.id} />
+                        </IonCol>
+                      ))
+                      :
+                      <h3 className="ion-text-center">
+                        Nothing to show yet, add your first apartment using the button below:
+                      </h3>
                   }
                 </IonRow>
               </IonGrid>
             </ResponsiveContent>
           </IonRow>
         </IonGrid>
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFab vertical={appCtx.apartments.length > 0 ? "bottom" : "center"} horizontal={appCtx.apartments.length > 0 ? "end" : "center"} slot="fixed">
           <IonFabButton onClick={() => setShowModal(true)}>
             <IonIcon icon={add} />
           </IonFabButton>
